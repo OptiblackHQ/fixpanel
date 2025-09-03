1dcdca965955ca06136e07594d08b3c2ae107cd0
syed sartaj <syedsartajahmed01@gmail.com>
2025-09-03 12:48:56 +0530

    done

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	basePath:'/fixpanel',
-	assetPrefix: '/fixpanel/',
-	// Uncomment the following lines if you want to set basePath and assetPrefix conditionally based on the environment
-	// basePath: process.env.NODE_ENV === 'production' ? '/fixpanel' : '',
+	basePath: process.env.NODE_ENV === 'production' ? '/fixpanel' : '',
 	output: "export",
 	trailingSlash: true, // Updated option name
 	images: {
 	  unoptimized: true, // Optional: Disable image optimization for static export
 	},
-	// assetPrefix: process.env.NODE_ENV === 'production' ? '/fixpanel' : '',
+	assetPrefix: process.env.NODE_ENV === 'production' ? '/fixpanel' : '',
 	reactStrictMode: false
 
 };
